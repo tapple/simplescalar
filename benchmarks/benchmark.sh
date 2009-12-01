e1be3b15caaf86ae21dda532306b6bd55f78967c
Matthew Fulmer <tapplek@gmail.com>
2009-11-30 21:34:40 -0500

    start a run3. Use a split L2 cache to prevent the buffer filling with instructions, and run go, as it seems to work again

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COMMAND='../simplesim-3.0/sim-outorder'
-OUTPUT_DIR='run2'
+OUTPUT_DIR='run3'
 
 TABLE_SIZES=$(echo {1..5})
 TABLE_SIZES=${@:-$TABLE_SIZES}
@@ -32,7 +32,7 @@ mkdir -p $OUTPUT_DIR
 for table_size_i in $TABLE_SIZES; do
 for table_assoc_i in {1..3}; do
 for buf_size_i in {1..3}; do
-for bench in {1..2}; do
+for bench in {1..3}; do
 
     buf_size=${BUF_SIZE[$buf_size_i]}
     table_size=${TABLE_SIZE[$table_size_i]}
@@ -42,11 +42,12 @@ for bench in {1..2}; do
     bench_name=${BENCHMARK[bench]}
     bench_opt=${!bench_name}
 
+    cache_opt="-cache:dl2 dl2:512:64:4:l -cache:il2 il2:512:64:4:l"
     buf_opt="-cache:pbuffer pbuffer:1:32:$buf_size:l"
     table_opt="-prefetch:trace ptrace:$table_sets:8:$table_assoc:l"
     output_file=$(printf '%s/ts%05d-ta%02d-bs%02d-%s.txt\n' $OUTPUT_DIR $table_size $table_assoc $buf_size $bench_name)
 
-    $COMMAND $table_opt $buf_opt $bench_opt 2> $output_file
+    echo $COMMAND $cache_opt $table_opt $buf_opt $bench_opt 2> $output_file
 
 done
 done
